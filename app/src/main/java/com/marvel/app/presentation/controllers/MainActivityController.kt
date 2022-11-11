de7db5f1e4f1d9548da42a65a35b5a4f3a20755a
micaelsn <micaelnascimentocc@gmail.com>
2022-11-10 21:33:50 -0300

    Fix rename presentation files and move them for your own folder, Add MainActivityController

---
+++ app/src/main/java/com/marvel/app/presentation/controllers/MainActivityController.kt
@@ -0,0 +1,4 @@
+package com.marvel.app.presentation.controllers
+
+class MainActivityController {
+}
--- app/src/main/java/com/marvel/app/presentation/presenters/CarouselListView.kt
+++ app/src/main/java/com/marvel/app/presentation/presenters/CarouselListView.kt
@@ -1,4 +1,4 @@
-package com.marvel.app
+package com.marvel.app.presentation.presenters
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.marvel.app.R
 import com.marvel.app.databinding.FragmentSecondBinding
 
 /**
