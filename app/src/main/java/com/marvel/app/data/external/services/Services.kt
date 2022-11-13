bb827befbaefd0f9baa5e8c0f8360f9d7d68c313
micaelsn <micaelnascimentocc@gmail.com>
2022-11-12 22:50:27 -0300

    Fix remane Requests to Services

--- app/src/main/java/com/marvel/app/data/external/services/Services.kt
+++ app/src/main/java/com/marvel/app/data/external/services/Services.kt
@@ -1,10 +1,10 @@
-package com.marvel.app.data.external.requests
+package com.marvel.app.data.external.services
 
 import com.marvel.app.data.infra.models.ResponseBody
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-interface Requests {
+interface Services {
     @GET("/v1/public/characters")
     suspend fun getCharacters(
         @Query("limit") limit: Int,
